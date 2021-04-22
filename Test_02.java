6426a620e1bf3a99133b42021f4362fece869b18
shinji0320 <shinji.blue.one@gmail.com>
2021-04-22 17:30:13 +0900

    文字列の指定

--- Test_02.java
+++ Test_02.java
@@ -4,10 +4,20 @@ class Test_02 {
     int var01 = 2;
     int var_01 = 3;
     int var$01 = 4;
+    int VAR = 5;
+    // int var-01 = 5;
+    // int 01var = 6;
+    char var02 = 'c';
+    String VAR02 = "hello world";
     
     System.out.println(var);
     System.out.println(var01);
     System.out.println(var_01);
     System.out.println(var$01);
+    System.out.println(VAR);
+    // System.out.println(var-01);
+    // System.out.println(01var);
+    System.out.println(var02);
+    System.out.println(VAR02);
   }
 }
