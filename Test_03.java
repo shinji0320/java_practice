903f1a1e8bcb7d405de9f73bb24915b73b852746
shinji0320 <shinji.blue.one@gmail.com>
2021-04-23 06:19:39 +0900

    比較演算

--- Test_03.java
+++ Test_03.java
@@ -53,12 +53,22 @@ class Test_03 {
 
     int x = 10;
     int y = 2;
+    int z = 10;
 
     System.out.println(x+y);
     System.out.println(x-y);
     System.out.println(x*y);
     System.out.println(x/y);
     System.out.println(x%y);
+    System.out.println(x > y);
+    System.out.println(x < y);
+
+    System.out.println(x >= y);
+    System.out.println(x <= y);
+    System.out.println(x <= z);
+
+    System.out.println(x == y);
+    System.out.println(x != y);
 
   }
 }
