185ff968b693b6d9b3a885209da1a52b50550db0
shinji0320 <shinji.blue.one@gmail.com>
2021-04-22 17:35:13 +0900

    boolean型の使い方

---
+++ Test_03.java
@@ -0,0 +1,12 @@
+class Test_03 {
+  public static void main(String[] args) {
+
+    int var_a = 10;
+    int var_b = 1;
+    boolean var_bool;
+
+    var_bool = (var_a < var_b);
+
+    System.out.println(var_bool);
+  }
+}
