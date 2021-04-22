b393c10a337b07288955307d022cd1525ec81bf0
shinji0320 <shinji.blue.one@gmail.com>
2021-04-22 17:45:54 +0900

    配列の書き方

--- Test_03.java
+++ Test_03.java
@@ -8,5 +8,26 @@ class Test_03 {
     var_bool = (var_a < var_b);
 
     System.out.println(var_bool);
+
+    String[] arr;
+    arr = new String[3];
+
+    arr[0]="sato";
+    arr[1]="suzuki";
+    arr[2]="tanaka";
+    // arr[3]="aaa";
+
+    System.out.println(arr[0]);
+    System.out.println(arr[1]);
+    System.out.println(arr[2]);
+    // System.out.println(arr[3]);
+
+    String[] arr1 = {"sato","suzuki","tanaka"};
+
+    arr1[1] = "kato";
+
+    System.out.println(arr1[0]);
+    System.out.println(arr1[1]);
+    System.out.println(arr1[2]);
   }
 }
