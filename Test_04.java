d188570d6f927b70070cdcc91dbd8772bdb699e4
shinji0320 <shinji.blue.one@gmail.com>
2021-04-23 07:07:08 +0900

    ループ処理

--- Test_04.java
+++ Test_04.java
@@ -9,5 +9,35 @@ class Test_04 {
     } else {
       System.out.println("child");
     }
+
+    // for(int i = 0; i <= 4; i++){
+    //   System.out.println(i);
+    // }
+
+    // for(int i = 0; i <= 4; i++){
+    //   if(i==3){
+    //     break;
+    //   }
+    //   System.out.println(i);
+    // }
+    // for(int i = 0; i <= 4; i++) {
+    //   if(i==3){
+    //     continue;
+    //   }
+    //   System.out.println(i);
+    // }
+    // for(int i = 0; i <= 2; i++){
+    //   for(int j = 0; j<= 2; j++ ){
+    //     System.out.println(i + "-" + j);
+    //   }
+    // }
+
+    int arr[] = {2,4,6,8,10};
+    int sum = 0;
+
+    for( int i =0; i <= 4; i++ ) {
+      sum += arr[i];
+    }
+    System.out.println(sum);
   }
 }
