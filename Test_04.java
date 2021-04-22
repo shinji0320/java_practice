849ce6e2fa3aa207096a61300e1f72a60726dc4c
shinji0320 <shinji.blue.one@gmail.com>
2021-04-23 07:28:59 +0900

    オブジェクト作成

--- Test_04.java
+++ Test_04.java
@@ -1,14 +1,36 @@
-class Test_04 {
-  public static void main(String[] args) {
-    int age = 0;
+class Student {
+  String name = "sato";
 
-    if (age >= 20){
-      System.out.println("adult");
-    } else if(age == 0){
-      System.out.println("baby");
-    } else {
-      System.out.println("child");
-    }
+  public void calculateAvg(int math, int english) {
+    System.out.println((math + english) / 2);
+  }
+}
+
+public class Test_04 {
+  public static void main(String[] args) {
+    Student a001 = new Student();
+    a001.name = "aaa";
+    System.out.println(a001.name);
+    a001.calculateAvg(90,80);
+
+    Student a002 = new Student();
+    a002.name = "suzuki";
+    System.out.println(a002.name);
+  }
+}
+
+
+// class Test_04 {
+//   public static void main(String[] args) {
+//     int age = 0;
+
+//     if (age >= 20){
+//       System.out.println("adult");
+//     } else if(age == 0){
+//       System.out.println("baby");
+//     } else {
+//       System.out.println("child");
+//     }
 
     // for(int i = 0; i <= 4; i++){
     //   System.out.println(i);
@@ -32,12 +54,12 @@ class Test_04 {
     //   }
     // }
 
-    int arr[] = {2,4,6,8,10};
-    int sum = 0;
+//     int arr[] = {2,4,6,8,10};
+//     int sum = 0;
 
-    for( int i =0; i <= 4; i++ ) {
-      sum += arr[i];
-    }
-    System.out.println(sum);
-  }
-}
+//     for( int i =0; i <= 4; i++ ) {
+//       sum += arr[i];
+//     }
+//     System.out.println(sum);
+//   }
+// }
