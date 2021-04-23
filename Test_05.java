1dd91c927b484f54ab5e1dec7bbf89469cc74626
shinji0320 <shinji.blue.one@gmail.com>
2021-04-23 15:12:13 +0900

    ファイル分割

--- Test_05.java
+++ Test_05.java
@@ -1,4 +1,54 @@
-class Student {
+// class Student {
+//   String name;
+
+//   public double calculateAvg(double[] data) {
+//     double sum = 0;
+
+//     for( int i = 0; i < data.length; i++) {
+//       sum += data[i];
+//     }
+
+//     double avg = sum/data.length;
+//     return avg;
+//   }
+
+//   public String judge (double avg) {
+//     String result;
+//     if(avg >= 60) {
+//       result = "passed";
+//     } else {
+//       result = "failed";
+//     }
+//     return result;
+//   }
+// }
+
+// public class Test_05 {
+//   public static void main(String[] args) {
+//     Student a001 = new Student();
+//     a001.name = "sato";
+//     double[] data = { 70, 65, 50, 10, 30};
+
+//     double avg = a001.calculateAvg(data);
+//     String result = a001.judge(avg);
+
+//     System.out.println(avg);
+//     System.out.println(a001.name + " " + result);
+
+
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+class Food {
   String name;
 
   public double calculateAvg(double[] data) {
@@ -15,9 +65,9 @@ class Student {
   public String judge (double avg) {
     String result;
     if(avg >= 60) {
-      result = "passed";
+      result = "美味しい";
     } else {
-      result = "failed";
+      result = "んー";
     }
     return result;
   }
@@ -25,16 +75,14 @@ class Student {
 
 public class Test_05 {
   public static void main(String[] args) {
-    Student a001 = new Student();
-    a001.name = "sato";
-    double[] data = { 70, 65, 50, 10, 30};
+    Food sushi = new Food();
+    sushi.name = "tuna";
+    double[] data = { 70, 65, 90, 90, 90};
 
-    double avg = a001.calculateAvg(data);
-    String result = a001.judge(avg);
+    double avg = sushi.calculateAvg(data);
+    String result = sushi.judge(avg);
 
     System.out.println(avg);
-    System.out.println(a001.name + " " + result);
-
-
+    System.out.println(sushi.name + " " + result);
   }
 }
